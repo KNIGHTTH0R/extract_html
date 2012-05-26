2d624ad38864ed6d503d057b76983c22406797cd
wensong he <snowhws@126.com>
2012-05-26 19:35:22 +0800

    尝试独立出评论，36kr通过

--- extract_html.py
+++ extract_html.py
@@ -3,7 +3,9 @@ import sys
 from bs4 import BeautifulSoup,Comment,NavigableString
 
 __DEBUG_OUTPUT__=False
-HTML_STOP_TAG={"textarea":1,"iframe":1,"script":1,"input":1}
+HTML_STOP_TAG={"textarea":1,"iframe":1,"script":1,"input":1,"style":1}
+HTML_CONTENT_TAG={"div":1,"article":1}
+HTML_COMMENTS_TAG={"li":1,"ul":1,"ol":1,"dl":1}
 
 class ExtractHtml:
     def __init__(self, doc , DEBUG=False):
@@ -12,7 +14,7 @@ class ExtractHtml:
         '''@doc : original html document(coding utf8)'''
         '''@return : class itself'''
         __DEBUG_OUTPUT__=DEBUG
-        self.__result_dict={"STATE":"False","title":"","content":""}
+        self.__result_dict={"STATE":"False","title":"","content":"","comment":""}
         self.__density=0.5
         self.__coding="utf8"
         self.__parser="html5lib"        
@@ -57,38 +59,67 @@ class ExtractHtml:
         except:
             return False
 
-    def __recursion_get_most_important_child_block(self, parent):
+    def __is_comment_tag(self,parent):
+        if isinstance(parent, NavigableString):
+            return False
         if not self.__has_chidren(parent):
-            return parent
+            return False
+        tag_count=0.0
+        cm_count=0.0
+        for child in parent.children:
+            if not isinstance(child, NavigableString):
+                if child.name in HTML_COMMENTS_TAG:
+                    cm_count+=1.0
+            tag_count+=1.0
+        if cm_count/tag_count>=self.__density:
+            return True
+        return False
+
+    def __recursion_get_most_important_child_block(self, parent):
+        content_tag=parent
+        if not self.__has_chidren(parent):
+            return content_tag
         parent_len=float(len(parent.get_text()))
         if parent_len<=0:
-            return parent
+            return content_tag
         imp_len=0.0
         imp_tag=None
+        comments_len=0.0
+        comment_tag=None
         for child in parent.children:
             child_len=0.0
             if isinstance(child, NavigableString):
                 continue
-            else:
+            if child.name in HTML_COMMENTS_TAG:
+                continue
+            elif child.name in HTML_CONTENT_TAG:
                 child_len=float(len(child.get_text()))
             if child_len>imp_len:
+                if self.__is_comment_tag(child):
+                    if child_len>comments_len:
+                        comment_tag=child
+                        child_len=comments_len
+                    continue
                 imp_len=child_len
                 imp_tag=child
         if imp_len/parent_len>=self.__density:
             return self.__recursion_get_most_important_child_block(imp_tag)
-        return parent
+        return (content_tag, comment_tag)
 
     def __run_extractor(self):
-        most_import_block=None
+        (content_tag, comment_tag)=(None,None)
         try:
-            most_import_block=self.__recursion_get_most_important_child_block(self.__soup.body)
+            (content_tag, comment_tag)=self.__recursion_get_most_important_child_block(self.__soup.body)
         except Exception,e:
             if __DEBUG_OUTPUT__:
                 print >> sys.stderr,"Error: when __recursion_get_most_important_child_block - ",e
             return
-        if most_import_block!=self.__soup.body:
-            self.__result_dict["content"]=unicode(most_import_block).encode(self.__coding,"ignore")
+        if content_tag!=self.__soup.body:
             self.__result_dict["STATE"]="True"
+            if comment_tag!=None:
+                self.__result_dict["comment"]=unicode(comment_tag).encode(self.__coding,"ignore")
+                comment_tag.extract()
+            self.__result_dict["content"]=unicode(content_tag).encode(self.__coding,"ignore")
         
     def GetResultDict(self):
         return self.__result_dict
