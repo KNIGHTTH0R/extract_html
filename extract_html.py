a5e8708e6143001bfa75f71d76a26ccca7926a07
wensong he <snowhws@126.com>
2012-06-02 02:34:26 +0800

    投票算法+递归最重要（还未融合）

--- extract_html.py
+++ extract_html.py
@@ -1,16 +1,14 @@
 import string
 import sys
-from bs4 import BeautifulSoup,Comment,NavigableString
+from bs4 import BeautifulSoup,Comment,NavigableString,CData
 
 __DEBUG_OUTPUT__=False
 HTML_STOP_TAG={"textarea":1,"iframe":1,"script":1,"input":1,"style":1}
+HTML_H_TAG={"h1":0,"h2":0,"h3":0,"h4":0,"h5":0,"h6":0}
+HTML_CONTENT_SUBTAG={"p":1,"br":1,"b":1,"strong":1,"hr":1,"pre":1}
+#recusive
 HTML_NORMAL_TAG={"center":1}
 HTML_CONTENT_TAG={"div":1,"article":1}
-HTML_COMMENTS_PARENT_TAG={"ul":1,"ol":1,"dl":1}
-HTML_COMMENTS_CHILD_TAG={"li":1,"dt":1,"dt":1}
-HTML_DIV_COMMENTS_TAG_STR="div"
-HTML_P_TAG_STR="p"
-HTML_USER_TYPE_TAG_PREFIX="data-"
 
 class ExtractHtml:
     def __init__(self, doc , DEBUG=False):
@@ -20,19 +18,16 @@ class ExtractHtml:
         '''@return : class itself'''
         __DEBUG_OUTPUT__=DEBUG
         self.__result_dict={"STATE":"False","title":"","content":"","comment":"","summary":""}
-        self.__density=0.5
-        self.__comments_density=0.8
         self.__coding="utf8"
-        self.__parser="html5lib"        
+        self.__parser="html5lib"
+        self.__density=0.5
+        self.__content_density=0.5
         self.__soup=None
+        self.__H_TAG_MAX_NUM=5
         
-        if self.__beautifulsoup(doc):
-            fw=open("1.txt","w")
-            #fw.write(self.__soup.prettify("utf8"))
-            fw.close()
-            self.__run_extractor()
+        self.__beautifulsoup(doc)
 
-    def __is_empty_tag(self,tag,special_name=""):
+    def __is_empty_tag(self,tag,special_name_dict=None):
         if isinstance(tag,NavigableString):
             if len(tag.rstrip().strip())==0:
                 return True
@@ -46,8 +41,8 @@ class ExtractHtml:
                 if isinstance(tag.contents[0],NavigableString):
                     if len(tag.contents[0].rstrip().strip())==0:
                         state=True
-        if special_name!="":
-            if tag.name!=special_name:
+        if special_name_dict!=None:
+            if tag.name not in special_name_dict:
                 state=False
         return state
 
@@ -58,25 +53,106 @@ class ExtractHtml:
             extract_candidates=[]
             if lens>0:
                 for i in range(0,lens):
-                    if self.__is_empty_tag(children[i],HTML_P_TAG_STR):
+                    if self.__is_empty_tag(children[i],HTML_CONTENT_SUBTAG):
                         extract_candidates.append(children[i])
                     else:
                         break
                 i=lens-1
                 while i>=0:
-                    if self.__is_empty_tag(children[i],HTML_P_TAG_STR):
+                    if self.__is_empty_tag(children[i],HTML_CONTENT_SUBTAG):
                         extract_candidates.append(children[i])
                     else:
                         break
                     i-=1
                 for tag in extract_candidates:
                     tag.extract()
-                        
-    def __extract_stop_tag(self):
-        all_tags=list(self.__soup.descendants)
+
+    def __has_children(self,tag):
+        try:
+            tag.children
+            return True
+        except:
+            return False
+
+    def __get_content_tag_len(self,root_tag):
+        len_count=0
+        for child_tag in root_tag.children:
+            if isinstance(child_tag,NavigableString):
+                len_count+=len(unicode(child_tag).rstrip().strip().encode("gbk","ignore"))
+        for content_tag_name in HTML_CONTENT_SUBTAG:
+            candis=list(root_tag.find_all(content_tag_name))
+            for tag in candis:
+                len_count+=len(unicode(tag).encode("gbk","ignore"))
+        return len_count
+
+    def __recursion_get_most_important_child_block(self, parent):
+        content_tag=parent
+        if not self.__has_children(parent):
+            return content_tag
+        parent_len=float(len(unicode(parent.get_text()).encode("gbk","ignore")))
+        if parent_len<=0:
+            return content_tag
+        imp_len=0.0
+        imp_tag=None
+        for child in parent.children:
+            child_len=0.0
+            if isinstance(child, NavigableString):
+                continue
+            if child.name in HTML_CONTENT_TAG or child.name in HTML_NORMAL_TAG:
+                child_len=float(len(unicode(child.get_text()).replace(" ","").rstrip().strip().encode("gbk","ignore")))
+            if child_len>imp_len:
+                imp_len=child_len
+                imp_tag=child
+        if imp_len/parent_len>=self.__density:
+            return self.__recursion_get_most_important_child_block(imp_tag)
+        parent_len=float(len(unicode(parent).encode("gbk","ignore")))
+        print self.__get_content_tag_len(content_tag)/parent_len,content_tag.name
+        if self.__get_content_tag_len(content_tag)/parent_len<self.__content_density:
+            print "none",#,content_tag["class"]
+            return (content_tag,False)
+        return (content_tag,True)
+        
+    def __vote_to_content_tag(self):
+        content_candidate_dict={}
+        winner_tag=None
+        winner_num=0
+        for content_tag_name in HTML_CONTENT_SUBTAG:
+            candis=list(self.__soup.body.find_all(content_tag_name))
+            for tag in candis:
+                candidate=tag.parent
+                try:
+                    while candidate.name in HTML_CONTENT_SUBTAG:
+                        candidate=candidate.parent
+                except:
+                    pass
+                text_len=len(unicode(tag.get_text().replace(" ","").rstrip().strip()).encode("gbk","ignore"))
+                cand_num=text_len
+                if str(candidate.attrs) not in content_candidate_dict:
+                    cands_list=[]
+                    cands_list.append([candidate,text_len])
+                    content_candidate_dict[str(candidate.attrs)]=cands_list
+                else:
+                    cand_list=content_candidate_dict[str(candidate.attrs)]
+                    lens=len(cand_list)
+                    _find=False
+                    for index in range(0,lens):
+                        if cand_list[index][0]==candidate:
+                            cand_list[index][1]+=text_len
+                            cand_num=cand_list[index][1]
+                            _find=True
+                            break
+                    if not _find:
+                        content_candidate_dict[str(candidate.attrs)].append([candidate,text_len])
+                if cand_num>winner_num:
+                    winner_tag=candidate
+                    winner_num=cand_num
+        return winner_tag
+    
+    def __extract_stop_tag(self, content_tag):
+        all_tags=list(content_tag.descendants)
         parent_list=[]
         for tag in all_tags:
-            if isinstance(tag,Comment):
+            if isinstance(tag,Comment) or isinstance(tag,CData):
                 parent_list.append(tag.parent)
                 tag.extract()
             elif isinstance(tag, NavigableString):
@@ -91,6 +167,16 @@ class ExtractHtml:
             if self.__is_empty_tag(parent):
                 parent.extract()
 
+    def __get_h_tag_num(self):
+        h_tag_count=0
+        h_tag_count+=len(list(self.__soup.body.find_all("h1")))
+        h_tag_count+=len(list(self.__soup.body.find_all("h2")))
+        h_tag_count+=len(list(self.__soup.body.find_all("h3")))
+        h_tag_count+=len(list(self.__soup.body.find_all("h4")))
+        h_tag_count+=len(list(self.__soup.body.find_all("h5")))
+        h_tag_count+=len(list(self.__soup.body.find_all("h6")))
+        return h_tag_count
+
     def __beautifulsoup(self,doc):
         '''using beautifulsoup parser the html doc'''
         try:
@@ -99,116 +185,41 @@ class ExtractHtml:
             if __DEBUG_OUTPUT__:
                 print >> sys.stderr,"Error: when __beautifulsoup1 - ",e
             return False
-        self.__extract_stop_tag()
         try:
             self.__result_dict["title"]=str(self.__soup.title.next_element.encode(self.__coding)).rstrip().strip()
+        except Exception,e:
+            if __DEBUG_OUTPUT__:
+                print >> sys.stderr,"Error: when __beautifulsoup2 title - ",e
+        try:
             self.__result_dict["content"]=self.__soup.body.encode(self.__coding)
         except Exception,e:
             if __DEBUG_OUTPUT__:
                 print >> sys.stderr,"Error: when __beautifulsoup2 - ",e
-            #return False
+        self.__extract_stop_tag(self.__soup.body)
+        (content_tag,succ)=(None,False)
+        #if self.__get_h_tag_num()>=self.__H_TAG_MAX_NUM:
+        #    (content_tag,succ)=self.__recursion_get_most_important_child_block(self.__soup.body)
+        #if not succ:
+        content_tag=self.__vote_to_content_tag()
+        #    if content_tag!=None:
+        #        if float(len(vote_content_tag))/len(content_tag)>=self.__density:
+        #            content_tag=vote_content_tag
+        #    else:
+        #        content_tag=vote_content_tag
+        if content_tag==None:
+            return False
+        if self.__has_children(content_tag):
+            self.__head_tail_normalization(content_tag)
+            self.__get_summary(content_tag)
+            self.__result_dict["STATE"]="True"
+            self.__result_dict["content"]=unicode(content_tag).encode(self.__coding,"ignore")
         return True
-
-    def __has_children(self,tag):
-        try:
-            tag.children
-            return True
-        except:
-            return False
-
-    def __attrs_is_equal(self,attrs1,attrs2):
-        if len(attrs1)!=len(attrs2):
-            return False
-        _equal=True
-        for key in attrs1:
-            if key not in attrs2:
-                return False
-            if not key.startswith(HTML_USER_TYPE_TAG_PREFIX):
-                if attrs1[key]!=attrs2[key]:
-                    return False
-        return True
-
-    def __is_comment_tag(self,parent):
-        if isinstance(parent, NavigableString):
-            return False
-        if not self.__has_children(parent):
-            return False
-        if parent.name in HTML_COMMENTS_PARENT_TAG:
-            return True
-        tag_count=0.0
-        list_count=0.0
-        div_cm_count=0.0
-        div_cm_attrs=None
-        for child in parent.children:
-            if not isinstance(child, NavigableString):
-                if child.name==HTML_DIV_COMMENTS_TAG_STR:
-                    if div_cm_attrs==None:
-                        div_cm_attrs=child.attrs
-                    elif self.__attrs_is_equal(div_cm_attrs,child.attrs):
-                        div_cm_count+=1.0
-                elif child.name in HTML_COMMENTS_PARENT_TAG:
-                    list_count+=1.0
-            tag_count+=1.0
-        if (list_count/tag_count)>=self.__density:
-            return True
-        if (div_cm_count/tag_count)>=self.__comments_density:
-            return True
-        return False
-
-    def __recursion_get_most_important_child_block(self, parent):
-        content_tag=parent
-        if not self.__has_children(parent):
-            return content_tag
-        parent_len=float(len(parent.get_text()))
-        if parent_len<=0:
-            return content_tag
-        imp_len=0.0
-        imp_tag=None
-        comments_len=0.0
-        comment_tag=None
-        for child in parent.children:
-            child_len=0.0
-            if isinstance(child, NavigableString):
-                continue
-            if child.name in HTML_COMMENTS_CHILD_TAG:#skip, just handle upper floor
-                continue
-            elif child.name in HTML_CONTENT_TAG or child.name in HTML_NORMAL_TAG:
-                child_len=float(len(unicode(child.get_text().replace(" ","").encode("gbk","ignore"))))
-            if child_len>imp_len:
-                if self.__is_comment_tag(child):
-                    if child_len>comments_len:
-                        comment_tag=child
-                        child_len=comments_len
-                    continue
-                imp_len=child_len
-                imp_tag=child
-        if imp_len/parent_len>=self.__density:
-            return self.__recursion_get_most_important_child_block(imp_tag)
-        return (content_tag, comment_tag)
 
     def __get_summary(self,content_tag):
         try:
-            self.__result_dict["summary"]=unicode(content_tag.get_text()[:40]).encode(self.__coding,"ignore")
+            self.__result_dict["summary"]=unicode(content_tag.get_text()[:80]).encode(self.__coding,"ignore")
         except:
             pass
-    
-    def __run_extractor(self):
-        (content_tag, comment_tag)=(None,None)
-        try:
-            (content_tag, comment_tag)=self.__recursion_get_most_important_child_block(self.__soup.body)
-        except Exception,e:
-            if __DEBUG_OUTPUT__:
-                print >> sys.stderr,"Error: when __recursion_get_most_important_child_block - ",e
-            return
-        if content_tag!=self.__soup.body:
-            self.__result_dict["STATE"]="True"
-            self.__get_summary(content_tag)
-            if comment_tag!=None:
-                self.__result_dict["comment"]=unicode(comment_tag).encode(self.__coding,"ignore")
-                comment_tag.extract()
-            self.__head_tail_normalization(content_tag)
-            self.__result_dict["content"]=unicode(content_tag).encode(self.__coding,"ignore")
-            
         
     def GetResultDict(self):
         return self.__result_dict
