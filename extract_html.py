65cd7197a8b61ae69ed5d81d79510a1c9f59308b
wensong he <snowhws@126.com>
2012-06-09 00:08:17 +0800

    改进投票算法，可提取分章节的长文章
    依然存在的缺陷是：不以<p>等作为内容标记的提取有问题

--- extract_html.py
+++ extract_html.py
@@ -5,10 +5,8 @@ from bs4 import BeautifulSoup,Comment,NavigableString,CData
 __DEBUG_OUTPUT__=False
 HTML_STOP_TAG={"textarea":1,"iframe":1,"script":1,"input":1,"style":1}
 HTML_H_TAG={"h1":0,"h2":0,"h3":0,"h4":0,"h5":0,"h6":0}
-HTML_CONTENT_SUBTAG={"p":1,"br":1,"b":1,"strong":1,"hr":1,"pre":1}
-#recusive
-HTML_NORMAL_TAG={"center":1}
-HTML_CONTENT_TAG={"div":1,"article":1}
+HTML_CONTENT_SUBTAG={"p":1,"br":1,"b":1,"strong":1,"hr":1,"pre":1,"blockquote":1}
+
 
 class ExtractHtml:
     def __init__(self, doc , DEBUG=False):
@@ -74,52 +72,64 @@ class ExtractHtml:
         except:
             return False
 
-    def __get_content_tag_len(self,root_tag):
-        len_count=0
-        for child_tag in root_tag.children:
-            if isinstance(child_tag,NavigableString):
-                len_count+=len(unicode(child_tag).rstrip().strip().encode("gbk","ignore"))
-        for content_tag_name in HTML_CONTENT_SUBTAG:
-            candis=list(root_tag.find_all(content_tag_name))
-            for tag in candis:
-                len_count+=len(unicode(tag).encode("gbk","ignore"))
-        return len_count
+    def __judgement_parent(self,child,parent):
+        if child==parent:
+            return True
+        if child.parent==self.__soup.body:
+            return False
+        return self.__judgement_parent(child.parent,parent)
 
-    def __recursion_get_most_important_child_block(self, parent):
-        content_tag=parent
-        if not self.__has_children(parent):
-            return content_tag
-        parent_len=float(len(unicode(parent.get_text()).encode("gbk","ignore")))
-        if parent_len<=0:
-            return content_tag
-        imp_len=0.0
-        imp_tag=None
-        for child in parent.children:
-            child_len=0.0
-            if isinstance(child, NavigableString):
-                continue
-            if child.name in HTML_CONTENT_TAG or child.name in HTML_NORMAL_TAG:
-                child_len=float(len(unicode(child.get_text()).replace(" ","").rstrip().strip().encode("gbk","ignore")))
-            if child_len>imp_len:
-                imp_len=child_len
-                imp_tag=child
-        if imp_len/parent_len>=self.__density:
-            return self.__recursion_get_most_important_child_block(imp_tag)
-        parent_len=float(len(unicode(parent).encode("gbk","ignore")))
-        print self.__get_content_tag_len(content_tag)/parent_len,content_tag.name
-        if self.__get_content_tag_len(content_tag)/parent_len<self.__content_density:
-            print "none",#,content_tag["class"]
-            return (content_tag,False)
-        return (content_tag,True)
+    def __judgement_winner(self,winner_tag,ptag_candidate_dict):
+        multi_ptag_candidate=[]
+        for attrs in ptag_candidate_dict:
+            for [candidate,num] in ptag_candidate_dict[attrs]:
+                if num>1 and candidate!=winner_tag:
+                    multi_ptag_candidate.append(candidate)
+        if len(multi_ptag_candidate)==0:
+            return winner_tag
+        _find=False
+        pwinner_tag=winner_tag.parent
+        max_pnum=0
+        while pwinner_tag!=self.__soup.body:
+            pnum=0
+            for cand in multi_ptag_candidate:
+                if self.__judgement_parent(cand,pwinner_tag):
+                    pnum+=1
+            if pnum>max_pnum:
+                max_pnum=pnum
+            if pnum==len(multi_ptag_candidate):
+                _find=True
+                break
+            pwinner_tag=pwinner_tag.parent
+        return pwinner_tag
         
     def __vote_to_content_tag(self):
         content_candidate_dict={}
+        ptag_candidate_dict={}
         winner_tag=None
         winner_num=0
+        second_tag=None
+        second_num=0
         for content_tag_name in HTML_CONTENT_SUBTAG:
             candis=list(self.__soup.body.find_all(content_tag_name))
             for tag in candis:
                 candidate=tag.parent
+                if candidate.name not in HTML_CONTENT_SUBTAG and tag.name=="p":
+                    if str(candidate.attrs) not in ptag_candidate_dict:
+                        cands_list=[]
+                        cands_list.append([candidate,1])
+                        ptag_candidate_dict[str(candidate.attrs)]=cands_list
+                    else:
+                        cand_list=ptag_candidate_dict[str(candidate.attrs)]
+                        lens=len(cand_list)
+                        _find=False
+                        for index in range(0,lens):
+                            if cand_list[index][0]==candidate:
+                                cand_list[index][1]+=1
+                                _find=True
+                                break
+                        if not _find:
+                            ptag_candidate_dict[str(candidate.attrs)].append([candidate,1])
                 try:
                     while candidate.name in HTML_CONTENT_SUBTAG:
                         candidate=candidate.parent
@@ -146,7 +156,7 @@ class ExtractHtml:
                 if cand_num>winner_num:
                     winner_tag=candidate
                     winner_num=cand_num
-        return winner_tag
+        return self.__judgement_winner(winner_tag,ptag_candidate_dict)
     
     def __extract_stop_tag(self, content_tag):
         all_tags=list(content_tag.descendants)
@@ -166,16 +176,6 @@ class ExtractHtml:
                 continue
             if self.__is_empty_tag(parent):
                 parent.extract()
-
-    def __get_h_tag_num(self):
-        h_tag_count=0
-        h_tag_count+=len(list(self.__soup.body.find_all("h1")))
-        h_tag_count+=len(list(self.__soup.body.find_all("h2")))
-        h_tag_count+=len(list(self.__soup.body.find_all("h3")))
-        h_tag_count+=len(list(self.__soup.body.find_all("h4")))
-        h_tag_count+=len(list(self.__soup.body.find_all("h5")))
-        h_tag_count+=len(list(self.__soup.body.find_all("h6")))
-        return h_tag_count
 
     def __beautifulsoup(self,doc):
         '''using beautifulsoup parser the html doc'''
@@ -197,15 +197,7 @@ class ExtractHtml:
                 print >> sys.stderr,"Error: when __beautifulsoup2 - ",e
         self.__extract_stop_tag(self.__soup.body)
         (content_tag,succ)=(None,False)
-        #if self.__get_h_tag_num()>=self.__H_TAG_MAX_NUM:
-        #    (content_tag,succ)=self.__recursion_get_most_important_child_block(self.__soup.body)
-        #if not succ:
         content_tag=self.__vote_to_content_tag()
-        #    if content_tag!=None:
-        #        if float(len(vote_content_tag))/len(content_tag)>=self.__density:
-        #            content_tag=vote_content_tag
-        #    else:
-        #        content_tag=vote_content_tag
         if content_tag==None:
             return False
         if self.__has_children(content_tag):
